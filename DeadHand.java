750698aeaa321ad8a5ca35bea2bda6a01ed7c424
Alex Vargas <avargas.rava@gmail.com>
2017-11-22 16:49:56 -0700

    Add files via upload
    First version of attackers

---
+++ DeadHand.java
@@ -0,0 +1,99 @@
+package apiaryparty;
+
+import java.util.Random;
+
+/**
+ * Attacker agent based on the ApiaryProject: https://github.com/osveliz/ApiaryParty.
+ * Attacker will chose randomly a node, if the SV is less than the threshold it will perform a normal attack
+ * If the SV is greater than the threshold it will scan other available node randomly
+ * If no node is available given the threshold, it will perfom a super attack on the node with the greater SV if possible
+ * If not, it will perform a normal attack in the node that has the smallest SV.
+ * IMPORTANT NOTE: 	Your attacker object will be recreated for every action. Because of this,
+ * 					model your Attacker to only make a decision on the current information. Do
+ * 					not try to use variables that will carry on in to the next makeSingleAction()
+ * 
+ * Make use of the three protected variables inherited from Attacker. These variables include:
+ * protected ArrayList&lt;Node&gt; capturedNodes - a list of the already captured nodes
+ * protected ArrayList&lt;Node&gt; availableNodes - a list of the available nodes for attacking and probing.
+ * protected int budget - the current budget of the Attacker. Be careful that your next move will not cost more than your budget.
+ * 
+ * @author Alex Vargas
+ */
+public class DeadHand extends Attacker {
+
+    private final static String attackerName = "DeadHand";
+    
+    public Random r;
+
+    private final int threshold = 15;
+
+    /**
+     * Constructor
+     * @param defenderName defender's name
+     * @param graphFile graph to read
+     */
+	public DeadHand(String defenderName, String graphFile) {
+		super(attackerName, defenderName, graphFile);
+	}
+	/**
+	 * Default constructor do not change
+	 */
+	public DeadHand(){
+		super(attackerName);
+	}
+	
+	/**
+	 * If you need to initialize anything, do it  here
+	 */
+	protected void initialize(){
+		r = new Random();
+	}
+
+	/**
+	 * This is called by the game master while your agent still has funds. You return an action.
+	 * @return your action
+	 */
+	public AttackerAction makeAction() {
+		AttackerActionType type = AttackerActionType.ATTACK;
+		Node mNodeToAttack  = null;
+		
+		for(Node mNode : availableNodes){
+			if (mNode.getSv() == -1)
+			{
+				int nodeID = mNode.getNodeID();
+				type = AttackerActionType.PROBE_POINTS;
+				return new AttackerAction(type, nodeID);
+			}
+			if(mNode.getSv() < threshold){
+				type = AttackerActionType.ATTACK;
+				mNodeToAttack = mNode;
+				break;
+			}
+		}
+		if(mNodeToAttack == null){
+			if(Parameters.SUPERATTACK_RATE < budget){ //Perform superattack on strongest node
+				type = AttackerActionType.SUPERATTACK;
+				for(Node mNode : availableNodes){
+					if(mNodeToAttack == null || mNodeToAttack.getSv() < mNode.getSv())
+						mNodeToAttack = mNode;
+				}
+			}else{ //Perform normal attack on weakest node
+				type = AttackerActionType.ATTACK;
+				for(Node mNode : availableNodes){
+					if(mNodeToAttack == null || mNodeToAttack.getSv() > mNode.getSv())
+						mNodeToAttack = mNode;
+				}
+			}
+		}
+		int nodeId = mNodeToAttack.getNodeID();
+		return new AttackerAction(type, nodeId);
+	}
+
+	/**
+	 * The game master is giving you the result of the action.
+	 * @param  lastNode the node successfully attacked
+	 */
+	protected void result(Node lastNode) {
+		
+	}
+}
---
+++ HotHead.java
@@ -0,0 +1,67 @@
+package apiaryparty;
+
+import java.util.Random;
+
+/**
+ * Attacker agent based on the ApiaryProject: https://github.com/osveliz/ApiaryParty.
+ * Attacker will check how many super attacks he can perform, if he can do more than 2 it will do a normal attack
+ * Else (budget <= (Parameter.SUPERATTACK_RATE * 2 + 1) && budget >= Parameter.SUPERATTACK_RATE) perform superattack
+ * IMPORTANT NOTE: 	Your attacker object will be recreated for every action. Because of this,
+ * 					model your Attacker to only make a decision on the current information. Do
+ * 					not try to use variables that will carry on in to the next makeSingleAction()
+ * 
+ * Make use of the three protected variables inherited from Attacker. These variables include:
+ * protected ArrayList&lt;Node&gt; capturedNodes - a list of the already captured nodes
+ * protected ArrayList&lt;Node&gt; availableNodes - a list of the available nodes for attacking and probing.
+ * protected int budget - the current budget of the Attacker. Be careful that your next move will not cost more than your budget.
+ * 
+ * @author Alex Vargas
+ */
+public class HotHead extends Attacker {
+
+    private final static String attackerName = "HotHead";
+    
+    public Random r;
+
+    /**
+     * Constructor
+     * @param defenderName defender's name
+     * @param graphFile graph to read
+     */
+	public HotHead(String defenderName, String graphFile) {
+		super(attackerName, defenderName, graphFile);
+	}
+	/**
+	 * Default constructor do not change
+	 */
+	public HotHead(){
+		super(attackerName);
+	}
+	
+	/**
+	 * If you need to initialize anything, do it  here
+	 */
+	protected void initialize(){
+		r = new Random();
+	}
+
+	/**
+	 * This is called by the game master while your agent still has funds. You return an action.
+	 * @return your action
+	 */
+	public AttackerAction makeAction() {
+		AttackerActionType type = AttackerActionType.ATTACK;
+		if(budget <= (Parameters.SUPERATTACK_RATE * 2 + 1) && budget >= Parameters.SUPERATTACK_RATE)
+			type = AttackerActionType.SUPERATTACK;
+		int nodeId = availableNodes.get(r.nextInt(availableNodes.size())).getNodeID();
+		return new AttackerAction(type, nodeId);
+	}
+
+	/**
+	 * The game master is giving you the result of the action.
+	 * @param  lastNode the node successfully attacked
+	 */
+	protected void result(Node lastNode) {
+		
+	}
+}
